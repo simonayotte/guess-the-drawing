acc42b67ccb7d18ea53dc6590dca0254f6a7c72b
Julien <desalliers.julien@gmail.com>
2021-01-27 21:40:10 -0500

    [JD] (dev) ajout de lifecyle live data

--- client_leger/app/src/main/java/com/example/client_leger/ClientLeger.kt
+++ client_leger/app/src/main/java/com/example/client_leger/ClientLeger.kt
@@ -3,6 +3,8 @@ package com.example.client_leger
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
 
+//Cette classe est nécessaire pour le fonctionnement de Hilt. Ne pas toucher s.v.p
+
 @HiltAndroidApp
 class ClientLeger: Application() {
 }
