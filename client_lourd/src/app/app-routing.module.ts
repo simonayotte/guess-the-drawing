1c6cf62016c72b4e06139d38165b65e5de035255
mwsadler <webersadler.mark@gmail.com>
2021-01-31 00:24:00 -0500

    [MWS] (hot): Electron - Fixe bug where page not found show

--- client_lourd/src/app/app-routing.module.ts
+++ client_lourd/src/app/app-routing.module.ts
@@ -4,13 +4,12 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { DrawingMainComponent } from './components/app/drawing-main/drawing-main.component';
 import { HomeComponent } from './components/app/interface/home/home.component';
-import { PageNotFoundComponent } from './components/app/page-not-found/page-not-found.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'draw', component: DrawingMainComponent},
-  { path: '**', component: PageNotFoundComponent }
+  { path: '**', component: HomeComponent }
 ];
 
 @NgModule({
