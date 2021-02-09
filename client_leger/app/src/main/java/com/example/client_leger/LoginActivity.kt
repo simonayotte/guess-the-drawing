5fa3436189f15fcd1d8a03c97060dde6451d1959
Julien <desalliers.julien@gmail.com>
2021-02-08 22:05:43 -0500

    [JD] (dev) post fonctionne, mais pas de body

--- client_leger/app/src/main/java/com/example/client_leger/LoginActivity.kt
+++ client_leger/app/src/main/java/com/example/client_leger/LoginActivity.kt
@@ -8,13 +8,18 @@ import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.lifecycle.ViewModelProvider
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.get
 import com.example.client_leger.databinding.ActivityLoginBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding: ActivityLoginBinding = DataBindingUtil.setContentView(this, R.layout.activity_login)
         val viewModel = ViewModelProvider(this).get(LoginViewModel::class.java)
+
         binding.viewmodel = viewModel
         binding.lifecycleOwner = this
         val background = findViewById<ConstraintLayout>(R.id.loginColorChange)
@@ -24,8 +29,10 @@ class LoginActivity : AppCompatActivity() {
         frameAnimation.start()
 
         viewModel.successfulLogin.observe(this, Observer {
-            val intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
+            if(it) {
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+            }
         })
 
         viewModel.showSignUp.observe(this, Observer {
--- client_leger/app/src/main/java/com/example/client_leger/LoginViewModel.kt
+++ client_leger/app/src/main/java/com/example/client_leger/LoginViewModel.kt
@@ -1,19 +1,43 @@
 package com.example.client_leger
 
+import androidx.hilt.lifecycle.R
+import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.client_leger.model.LoginRepository
+import com.example.client_leger.model.LoginResponse
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class LoginViewModel : ViewModel() {
+@HiltViewModel
+class LoginViewModel @Inject constructor(
+    private val loginRepository: LoginRepository
+) : ViewModel() {
+
     val successfulLogin: MutableLiveData<Boolean> = MutableLiveData()
     val showSignUp: MutableLiveData<Boolean> = MutableLiveData()
-    val userName = MutableLiveData<String>()
-    val password = MutableLiveData<String>()
+    val userName = MutableLiveData<String>("Julien")
+    val password = MutableLiveData<String>("456")
+    val textErrorIsVisible = MutableLiveData<Boolean>(false)
+
     fun onClickSignUp(){
         showSignUp.value = true
     }
+
     fun onClickLogIn() {
-        //TODO: verify the credentials with the database here
-        // username and password are already bind to the view
-        successfulLogin.value = true
+        viewModelScope.launch {
+            val result = loginRepository.makeLoginRequest(userName.value!!, password.value!!)
+            when (result) {
+                is Result.Success<LoginResponse> -> {
+                    successfulLogin.value = true
+                }
+                else -> {
+                    successfulLogin.value = false
+                    textErrorIsVisible.value = true
+                }
+            }
+        }
     }
 }
