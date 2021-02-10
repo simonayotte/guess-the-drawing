f9f17808985b17faee00818f36ca93d8fb73821c
JulienDesalliers <36003332+JulienDesalliers@users.noreply.github.com>
2021-02-10 10:10:39 -0500

    [JD] (dev) retrait de dead code

--- client_leger/app/src/main/java/com/example/client_leger/MainActivity.kt
+++ client_leger/app/src/main/java/com/example/client_leger/MainActivity.kt
@@ -9,30 +9,8 @@ import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
-    // field injection
-    @Inject
-    lateinit var someClass: SomeClass
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        println(someClass.printSomething())
-    }
-}
-
-class SomeClass
-@Inject
-//constructor injection
-constructor(private val anotherClass: AnotherClass){
-    fun printSomething(): String {
-        return anotherClass.printSomethingElse()
-    }
-}
-
-class AnotherClass
-@Inject
-constructor(){
-    fun printSomethingElse(): String {
-        return "Yo it's printing something else !!"
     }
 }
